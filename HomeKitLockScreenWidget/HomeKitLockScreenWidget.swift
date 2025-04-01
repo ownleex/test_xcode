03fabf9972b9c1a5070ca5d10970560030423347
ownleex <145456737+ownleex@users.noreply.github.com>
2025-04-01 23:08:51 +0200

    change

--- HomeKitLockScreenWidget/HomeKitLockScreenWidget.swift
+++ HomeKitLockScreenWidget/HomeKitLockScreenWidget.swift
@@ -20,7 +20,7 @@ struct HomeKitLockScreenWidgetProvider: TimelineProvider {
     }
     
     func getTimeline(in context: Context, completion: @escaping (Timeline<HomeKitLockScreenWidgetEntry>) -> Void) {
-        let userDefaults = UserDefaults(suiteName: "group.com.mon_id.homekit")
+        let userDefaults = UserDefaults(suiteName: "group.perso.HomeKitLockScreenApp")
         let temp = userDefaults?.double(forKey: "temperature") ?? 0.0
         let hum = userDefaults?.double(forKey: "humidity") ?? 0.0
         
